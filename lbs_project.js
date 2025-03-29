c41236f373254471d8701bf5aad62003eaaa64db
a22chrfa <128893264+a22chrfa@users.noreply.github.com>
2025-03-29 04:15:38 +0100

    Update lbs_project.js to v6
    Added loop to iterate through multiple computations. Also noticed that the first iteration always executes slower.
    
    Added logging to csv file with fs and path. It now outputs a csv file with headers and values for each iteration.

--- lbs_project.js
+++ lbs_project.js
@@ -1,28 +1,20 @@
 (async () => {
-    console.clear();
-    const SEAL = require('node-seal');
-    const seal = await SEAL();
+    process.stdout.write("\x1Bc");
+    const fs = require('fs');
+    const path = require('path');
+    const outputFilePath = path.join(__dirname, 'results.csv');
 
     const GeographicLib = require("geographiclib");
     const geod = GeographicLib.Geodesic.WGS84;
 
-    // Set the scheme type and security level
-    const schemeType = seal.SchemeType.ckks;
-    const securityLevel = seal.SecurityLevel.tc128;
-    const n_polyModulusDegree = 4096;
-    const modulusChain = [36, 36, 37];
-    const scale = Math.pow(2, 20);
-
-    const parms = seal.EncryptionParameters(schemeType);
-    parms.setPolyModulusDegree(n_polyModulusDegree);
-    parms.setCoeffModulus(seal.CoeffModulus.Create(n_polyModulusDegree, Int32Array.from(modulusChain)));
-    const context = seal.Context(parms, false, securityLevel);
-
-    if (!context.parametersSet()) {
-        throw new Error(
-            'Encryption parameters not valid'
-        );
+    if (fs.existsSync(outputFilePath)) {
+        fs.unlinkSync(outputFilePath);
+        console.log(`Deleting existing file: ${outputFilePath}`);
     }
+    console.log(`Creating new file: ${outputFilePath}`);
+
+    const headers = `"Iteration","Encrypted distance (km)","Unencrypted distance (km)","Karney distance ref (km)"\n`;
+    fs.appendFileSync(outputFilePath, headers, 'utf8');
 
     /* ------ https://github.com/LenaSYS/Random-Number-Generator/blob/master/seededrandom.js ------ */
     // Jenkins small fast with replace .random() for deterministic random numbers
@@ -47,7 +39,28 @@
         Math.randSeed = seed;
         for (let i = 0; i < 7; i++) Math.random();
     }
-    Math.setSeed(14);
+
+    /* ------------------------------- Encryption settings and instance ------------------------------- */
+    const SEAL = require('node-seal');
+    const seal = await SEAL();
+
+    // Set the scheme type and security level
+    const schemeType = seal.SchemeType.ckks;
+    const securityLevel = seal.SecurityLevel.tc128;
+    const n_polyModulusDegree = 4096;
+    const modulusChain = [36, 36, 37];
+    const scale = Math.pow(2, 20);
+
+    const parms = seal.EncryptionParameters(schemeType);
+    parms.setPolyModulusDegree(n_polyModulusDegree);
+    parms.setCoeffModulus(seal.CoeffModulus.Create(n_polyModulusDegree, Int32Array.from(modulusChain)));
+    const context = seal.Context(parms, false, securityLevel);
+
+    if (!context.parametersSet()) {
+        throw new Error(
+            'Encryption parameters not valid'
+        );
+    }
 
     const ckksEncoder = seal.CKKSEncoder(context);
     const keyGenerator = seal.KeyGenerator(context);
@@ -56,105 +69,108 @@
     const encryptor = seal.Encryptor(context, publicKey);
     const decryptor = seal.Decryptor(context, secretKey);
     const evaluator = seal.Evaluator(context);
+
+    /* ------------------------------- Client side data and conversions ------------------------------- */
     const R = 6371; // Earth's radius in km
+    const max_iterations = 10;
 
-    //CLIENT A
-    let client_A_latitude = (Math.random() * 180 - 90).toFixed(5); //phi
-    let client_A_longitude = (Math.random() * 360 - 180).toFixed(5); //lambda
-    console.log("Client A lat: ", client_A_latitude);
-    console.log("Client A lon: ", client_A_longitude);
+    for (let i = 0; i < max_iterations; i++) {
+        Math.setSeed(i);
 
-    //to radians
-    let client_A_latitude_radian = client_A_latitude * (Math.PI / 180);
-    let client_A_longitude_radian = client_A_longitude * (Math.PI / 180);
+        //CLIENT A
+        let client_A_latitude = (Math.random() * 180 - 90).toFixed(5); //phi
+        let client_A_longitude = (Math.random() * 360 - 180).toFixed(5); //lambda
 
-    //convert to cartesian
-    let X_A = R * Math.cos(client_A_latitude_radian) * Math.cos(client_A_longitude_radian);
-    let Y_A = R * Math.cos(client_A_latitude_radian) * Math.sin(client_A_longitude_radian);
-    let Z_A = R * Math.sin(client_A_latitude_radian);
+        //to radians
+        let client_A_latitude_radian = client_A_latitude * (Math.PI / 180);
+        let client_A_longitude_radian = client_A_longitude * (Math.PI / 180);
 
-    //CLIENT B
-    let client_B_latitude = (Math.random() * 180 - 90).toFixed(5);
-    let client_B_longitude = (Math.random() * 360 - 180).toFixed(5);
-    console.log("Client B lat: ", client_B_latitude);
-    console.log("Client B lon: ", client_B_longitude);
+        //convert to cartesian
+        let X_A = R * Math.cos(client_A_latitude_radian) * Math.cos(client_A_longitude_radian);
+        let Y_A = R * Math.cos(client_A_latitude_radian) * Math.sin(client_A_longitude_radian);
+        let Z_A = R * Math.sin(client_A_latitude_radian);
 
-    //convert to radians
-    let client_B_latitude_radian = client_B_latitude * (Math.PI / 180);
-    let client_B_longitude_radian = client_B_longitude * (Math.PI / 180);
+        //CLIENT B
+        let client_B_latitude = (Math.random() * 180 - 90).toFixed(5);
+        let client_B_longitude = (Math.random() * 360 - 180).toFixed(5);
 
-    //convert to cartesian
-    let X_B = R * Math.cos(client_B_latitude_radian) * Math.cos(client_B_longitude_radian);
-    let Y_B = R * Math.cos(client_B_latitude_radian) * Math.sin(client_B_longitude_radian);
-    let Z_B = R * Math.sin(client_B_latitude_radian);
+        //convert to radians
+        let client_B_latitude_radian = client_B_latitude * (Math.PI / 180);
+        let client_B_longitude_radian = client_B_longitude * (Math.PI / 180);
 
-    //CLIENT A AND B ENCRYPTIONS
-    //store in array
-    let arr_X_A = Float64Array.from([X_A]);
-    let arr_Y_A = Float64Array.from([Y_A]);
-    let arr_Z_A = Float64Array.from([Z_A]);
-    let arr_X_B = Float64Array.from([X_B]);
-    let arr_Y_B = Float64Array.from([Y_B]);
-    let arr_Z_B = Float64Array.from([Z_B]);
+        //convert to cartesian
+        let X_B = R * Math.cos(client_B_latitude_radian) * Math.cos(client_B_longitude_radian);
+        let Y_B = R * Math.cos(client_B_latitude_radian) * Math.sin(client_B_longitude_radian);
+        let Z_B = R * Math.sin(client_B_latitude_radian);
 
-    //encode values
-    let encoded_X_A = ckksEncoder.encode(arr_X_A, scale);
-    let encoded_Y_A = ckksEncoder.encode(arr_Y_A, scale);
-    let encoded_Z_A = ckksEncoder.encode(arr_Z_A, scale);
-    let encoded_X_B = ckksEncoder.encode(arr_X_B, scale);
-    let encoded_Y_B = ckksEncoder.encode(arr_Y_B, scale);
-    let encoded_Z_B = ckksEncoder.encode(arr_Z_B, scale);
+        //CLIENT A AND B ENCRYPTIONS
+        //store in array
+        let arr_X_A = Float64Array.from([X_A]);
+        let arr_Y_A = Float64Array.from([Y_A]);
+        let arr_Z_A = Float64Array.from([Z_A]);
+        let arr_X_B = Float64Array.from([X_B]);
+        let arr_Y_B = Float64Array.from([Y_B]);
+        let arr_Z_B = Float64Array.from([Z_B]);
 
-    //encrypt encoded values
-    let encrypted_X_A = encryptor.encrypt(encoded_X_A);
-    let encrypted_Y_A = encryptor.encrypt(encoded_Y_A);
-    let encrypted_Z_A = encryptor.encrypt(encoded_Z_A);
-    let encrypted_X_B = encryptor.encrypt(encoded_X_B);
-    let encrypted_Y_B = encryptor.encrypt(encoded_Y_B);
-    let encrypted_Z_B = encryptor.encrypt(encoded_Z_B);
+        //encode values
+        let encoded_X_A = ckksEncoder.encode(arr_X_A, scale);
+        let encoded_Y_A = ckksEncoder.encode(arr_Y_A, scale);
+        let encoded_Z_A = ckksEncoder.encode(arr_Z_A, scale);
+        let encoded_X_B = ckksEncoder.encode(arr_X_B, scale);
+        let encoded_Y_B = ckksEncoder.encode(arr_Y_B, scale);
+        let encoded_Z_B = ckksEncoder.encode(arr_Z_B, scale);
 
-    //encrypted computations and decryptions
-    let encrypted_time_test_1 = process.hrtime(); //server side start
-    let encrypted_delta_x = evaluator.sub(encrypted_X_A, encrypted_X_B);
-    let encrypted_delta_y = evaluator.sub(encrypted_Y_A, encrypted_Y_B);
-    let encrypted_delta_z = evaluator.sub(encrypted_Z_A, encrypted_Z_B);
-    let encrypted_delta_x_sq = evaluator.multiply(encrypted_delta_x, encrypted_delta_x);
-    let encrypted_delta_y_sq = evaluator.multiply(encrypted_delta_y, encrypted_delta_y);
-    let encrypted_delta_z_sq = evaluator.multiply(encrypted_delta_z, encrypted_delta_z);
-    let encrypted_sum_sq_1 = evaluator.add(encrypted_delta_x_sq, encrypted_delta_y_sq);
-    let encrypted_sum_sq = evaluator.add(encrypted_sum_sq_1, encrypted_delta_z_sq);
-    let encrypted_time_test_2 = process.hrtime(encrypted_time_test_1); //server side stop
-    let encrypted_time = (encrypted_time_test_2[0] * 1000 + (encrypted_time_test_2[1] / 1000000));
+        //encrypt encoded values
+        let encrypted_X_A = encryptor.encrypt(encoded_X_A);
+        let encrypted_Y_A = encryptor.encrypt(encoded_Y_A);
+        let encrypted_Z_A = encryptor.encrypt(encoded_Z_A);
+        let encrypted_X_B = encryptor.encrypt(encoded_X_B);
+        let encrypted_Y_B = encryptor.encrypt(encoded_Y_B);
+        let encrypted_Z_B = encryptor.encrypt(encoded_Z_B);
 
-    let decrypted_sum = decryptor.decrypt(encrypted_sum_sq);
-    let decoded_sum = ckksEncoder.decode(decrypted_sum);
+        /* ------------------------------- Server side computations ------------------------------- */
+        //ENCRYPTED COMPUTATIONS
+        let encrypted_time_test_1 = process.hrtime(); //server side start
+        let encrypted_delta_x = evaluator.sub(encrypted_X_A, encrypted_X_B);
+        let encrypted_delta_y = evaluator.sub(encrypted_Y_A, encrypted_Y_B);
+        let encrypted_delta_z = evaluator.sub(encrypted_Z_A, encrypted_Z_B);
+        let encrypted_delta_x_sq = evaluator.multiply(encrypted_delta_x, encrypted_delta_x);
+        let encrypted_delta_y_sq = evaluator.multiply(encrypted_delta_y, encrypted_delta_y);
+        let encrypted_delta_z_sq = evaluator.multiply(encrypted_delta_z, encrypted_delta_z);
+        let encrypted_sum_sq_1 = evaluator.add(encrypted_delta_x_sq, encrypted_delta_y_sq);
+        let encrypted_sum_sq = evaluator.add(encrypted_sum_sq_1, encrypted_delta_z_sq);
+        let encrypted_time_test_2 = process.hrtime(encrypted_time_test_1); //server side stop
+        let encrypted_time = (encrypted_time_test_2[0] * 1000 + (encrypted_time_test_2[1] / 1000000));
 
-    let encrypted_distance_float = parseFloat(decoded_sum[0]);
+        let decrypted_sum = decryptor.decrypt(encrypted_sum_sq);
+        let decoded_sum = ckksEncoder.decode(decrypted_sum);
 
-    //after sent to client
-    let encrypted_distance = Math.sqrt(encrypted_distance_float);
+        let encrypted_distance_float = parseFloat(decoded_sum[0]);
 
-    //plaintext
-    let deltaX = X_A - X_B;
-    let deltaY = Y_A - Y_B;
-    let deltaZ = Z_A - Z_B;
+        //UNENCRYPTED COMPUTATIONS FOR COMPARISON
+        let encrypted_distance = Math.sqrt(encrypted_distance_float);
 
-    let squareSum = deltaX ** 2 + deltaY ** 2 + deltaZ ** 2;
-    let plaintext_root = Math.sqrt(squareSum);
+        //plaintext
+        let deltaX = X_A - X_B;
+        let deltaY = Y_A - Y_B;
+        let deltaZ = Z_A - Z_B;
 
-    // Karney (geographiclib)
-    let karney_result = geod.Inverse(
-        parseFloat(client_A_latitude),
-        parseFloat(client_A_longitude),
-        parseFloat(client_B_latitude),
-        parseFloat(client_B_longitude)
-    );
+        let squareSum = deltaX ** 2 + deltaY ** 2 + deltaZ ** 2;
+        let unencrypted_distance = Math.sqrt(squareSum);
 
+        // Karney (geographiclib)
+        let karney_result = geod.Inverse(
+            parseFloat(client_A_latitude),
+            parseFloat(client_A_longitude),
+            parseFloat(client_B_latitude),
+            parseFloat(client_B_longitude)
+        );
 
-    console.log("encrypted: ", encrypted_distance);
-    console.log("plaintext: ", plaintext_root);
-    console.log("Execution time: ", encrypted_time)
-    console.log("Karney", (karney_result.s12 / 1000).toFixed(5));
+        const csvData = `${i},${encrypted_distance.toFixed(5)},${unencrypted_distance.toFixed(5)},${((karney_result.s12 / 1000).toFixed(5))}\n`;
+        fs.appendFileSync(outputFilePath, csvData, 'utf8');
+        console.log("Row: ", i);
+
+    }
 }
 
 )();
